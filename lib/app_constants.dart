ade48784b660d8990c0fef87a18888b56118cc68
MK <103623651+MangeshKakade@users.noreply.github.com>
2023-05-10 17:56:15 +0530

    Update app_constants.dart

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
-  static const accountSID = 'AC4dee0da322bcef15f6745a70f4f90fdf';
-  static const authToken = '26ca1f9fb146cb2956a062f6b949028a';
+  static const accountSID = 'AC*********************';
+  static const authToken = '*************************';
   static const domainURL = 'https://conversations.twilio.com/v1';
   static late String _identity;
   static String get getIdentity => _identity;
